14b1b8769a283bf785aab4ac9f38cdc716c3c565
KATHEEJATHULKUBRA <rubyfathima16102001@gmail.com>
2025-05-07 14:56:38 +0530

    Initial commit from Replit

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,13 @@
 
-// postcss.config.js
-module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    process.env.NODE_ENV !== 'production' ? require('autoprefixer') : null
-  ].filter(Boolean)
-}
+// postcss.config.js (ESM)
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+const plugins = [
+  tailwindcss,
+  process.env.NODE_ENV !== 'production' ? autoprefixer : null
+].filter(Boolean);
+
+export default {
+  plugins
+};
