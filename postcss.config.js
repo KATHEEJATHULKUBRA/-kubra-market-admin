9eb25da12d7c017694cf46800677217f9170d422
KATHEEJATHULKUBRA <rubyfathima16102001@gmail.com>
2025-05-07 13:47:14 +0530

    Make autoprefixer optional in production builds

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+
+// postcss.config.js
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    process.env.NODE_ENV !== 'production' ? require('autoprefixer') : null
+  ].filter(Boolean)
 }
